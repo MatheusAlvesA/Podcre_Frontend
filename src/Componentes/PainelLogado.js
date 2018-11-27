d5b98a3d812a557393d3176bd61429d66af22824
Matheus Alves <matheuseejam@gmail.com>
2018-11-26 21:10:13 -0300

    Adicionando podcasts ao painel logado

--- src/Componentes/PainelLogado.js
+++ src/Componentes/PainelLogado.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import InfosUser from './InfosUser.js';
 import PainelUparPodcast from './PainelUparPodcast.js';
+import PainelEscutarPodcast from './PainelEscutarPodcast.js';
 const $ = window.$;
 
 export default class PainelLogado extends Component {
@@ -10,16 +11,17 @@ export default class PainelLogado extends Component {
 
     this.state = {
       "nome": "",
-      "email": ""
+      "email": "",
+      "listaPodcasts": []
     };
 
     this.buscarDados = this.buscarDados.bind(this);
   }
 
-
-componentDidMount() {
-  this.buscarDados();
-}
+  componentDidMount() {
+    this.buscarDados();
+    this.buscarDadosPodcasts();
+  }
 
   buscarDados() {
     $.ajax({
@@ -33,12 +35,54 @@ componentDidMount() {
           });
   }
 
+  buscarDadosPodcasts() {
+    $.ajax({
+             url: "https://podcre-223420.appspot.com/api/getPodcasts?nome="+this.props.nomeUser,
+             type: "GET",
+             success: (r) => {this.setState({"listaPodcasts": r.data});},
+             error: () => {setTimeout(this.buscarDados, 1000);}
+          });
+  }
+
+  toggle() {
+    if($("#painelPodcastsLogado").css("display") === "none")
+      $("#painelPodcastsLogado").css("display", "block");
+    else
+      $("#painelPodcastsLogado").css("display", "none");
+  }
 
   render() {
+
+    const lista = this.state.listaPodcasts.map(
+                    (epi) => {
+                      return <PainelEscutarPodcast
+                                nomeUser={epi.nome}
+                                nome={epi.nome}
+                                assunto={epi.assunto}
+                                likes={epi.n_likes}
+                                dislikes={epi.n_dislikes}
+                                blob={epi.key_blob}
+                                chave={epi.chave}
+
+                                key={epi.chave}
+                              />;
+                    });
+
     return (
       <div id="painelLogado" className="col-md-8">
         <InfosUser nomeUser={this.state.nome} email={this.state.email}/>
         <PainelUparPodcast nomeUser={this.props.nomeUser} email={this.state.email}/>
+        <div className="row" style={{"marginTop": "10px"}}>
+            <div className="col-9">
+              <h3>Seus podcasts:</h3>
+            </div>
+            <div className="col-3">
+              <button onClick={this.toggle} type="button" className="btn btn-outline-info">Mostrar</button>
+            </div>
+        </div>
+        <div className="row" id="painelPodcastsLogado" style={{"display": "none"}}>
+          {lista}
+        </div>
       </div>
     );
   }
